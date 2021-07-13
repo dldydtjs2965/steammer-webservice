75da263ab3d852dc31a6014c60510b37f67e9db6
dldydtjs2965 <dldydtjs8124@gmail.com>
2021-07-13 18:07:03 +0900

    [#7] 2021-07-13 google login update

--- src/main/java/com/steammer/web/IndexController.java
+++ src/main/java/com/steammer/web/IndexController.java
@@ -1,20 +1,31 @@
 package com.steammer.web;
 
+import com.steammer.config.auth.dto.SessionUser;
 import com.steammer.service.GamesService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final GamesService gamesService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model){
         model.addAttribute("games", gamesService.findAllPaging(0));
+
+        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if (user != null){
+            model.addAttribute("steammerUserName", user.getName());
+            System.out.println("userName"+user.getName());
+        }
         return "index";
     }
 }
